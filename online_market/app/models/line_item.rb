8dba2bf252d9b9bc261161ef206ed884dc72fbad
Ronan Carton <ronancarton7@gmail.com>
2012-07-16 21:26:07 +0100

    image tag

--- online_market/app/models/line_item.rb
+++ online_market/app/models/line_item.rb
@@ -2,7 +2,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   belongs_to :cart
-end
+
 
 def total_price    # via Agile Web Dev with rails
   product.price * quantity
